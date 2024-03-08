900853de192d6decf3f53d923ea1ce64b71d4464
Ruben Petit <rapetitc@gmail.com>
2024-03-08 18:50:11 -0300

    Entregable 1

---
+++ Entregable 1/app.js
@@ -0,0 +1,22 @@
+const readlineSync = require("readline-sync");
+
+function adivina_numero_secreto() {
+  const numero_secreto = Math.floor(Math.random() * 100) + 1;
+  let intentos = 0;
+
+  while (true) {
+      const numero_usuario = readlineSync.question("divina el número secreto entre 1 y 100: ");
+      intentos++;
+
+    if (numero_usuario < numero_secreto) {
+      console.log("El número es demasiado bajo. Intenta nuevamente.");
+    } else if (numero_usuario > numero_secreto) {
+      console.log("El número es demasiado alto. Intenta nuevamente.");
+    } else {
+      console.log(`Felicidades, has adivinado el número secreto en ${intentos} intentos!`);
+      break;
+    }
+  }
+}
+
+adivina_numero_secreto();
